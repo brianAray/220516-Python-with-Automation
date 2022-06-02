c299acd76aa774cebdd94290205c473ea567fc4a
DementedTiger <72563448+DementedTiger@users.noreply.github.com>
2022-06-02 12:31:00 -0400

    added more js and dom demos

--- demo/javascript_intro/basic_dom.js
+++ demo/javascript_intro/basic_dom.js
@@ -4,3 +4,22 @@ let div = document.querySelector("#div_id")
 
 console.log(header)
 console.log(div)
+
+
+let nameArray = ["Brian", "Greg", "James"]
+
+let nameDiv = document.createElement("div")
+
+function createHeader(name){
+    let nameHeader = document.createElement("h1")
+    nameHeader.innerText = name
+    return nameHeader
+}
+
+for (element of nameArray){
+    let header = createHeader(element)
+    nameDiv.append(header)
+}
+
+let body = document.querySelector("body")
+body.append(nameDiv)
--- demo/javascript_intro/basic_js.js
+++ demo/javascript_intro/basic_js.js
@@ -46,4 +46,82 @@ function printer(){
 }
 
 
-// console.log(funcString)
+{
+    let y = 2
+}
+
+var x = 10
+
+{
+    let x = 2
+}
+
+console.log(x)
+
+const a = 12
+    
+// a = "hello"
+
+// Arrays
+
+let cheeses = ['bleu', 'cheddar', 'parmesan']
+
+console.log(cheeses)
+console.log(cheeses[2])
+
+console.log(cheeses.length)
+cheeses[cheeses.length-1] = "american"
+console.log(cheeses)
+console.log(cheeses[cheeses.length-1])
+
+
+let weirdArray = []
+weirdArray[0] = "Duck"
+weirdArray[weirdArray.length] = "Cat"
+
+weirdArray.push('Person')
+console.log(weirdArray)
+
+
+let arr = [1,2,3,4,5,6,7,8,9]
+arr.splice(2, 1)
+console.log(arr)
+
+for (let i =0; i <arr.length; i++){
+    console.log(arr[i])
+}
+
+for (key in weirdArray){
+    console.log(weirdArray[key])
+}
+
+for (element of weirdArray){
+    console.log(element)
+}
+
+
+// Arrow functions
+
+let hello = () => console.log("Hello World")
+hello()
+
+let sum = (a , b) => {
+    let result = a + b
+    return "Sum is: " + result
+}
+
+console.log(sum(3, 5))
+
+let anon_func = function (){
+    console.log("This is anonymous")
+}
+
+anon_func()
+
+function funcRunner(functionToRun){
+    functionToRun()
+}
+
+funcRunner(anon_func)
+console.log(typeof(anon_func))
+
